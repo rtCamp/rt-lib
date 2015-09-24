d3bed6ad56dc1e78d283a0c5ac9b690ea1d5a73c
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2015-09-24 18:22:58 +0530

    Code formatted with updated editorconfig file

--- rt-mailbox/class-rt-mail-cron.php
+++ rt-mailbox/class-rt-mail-cron.php
@@ -23,26 +23,31 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 
 			add_action( 'init', array( $this, 'setup_schedule' ) );
 
-			register_deactivation_hook( $plugin_path_for_deactivate_cron, array( $this, 'disable_cron_on_deactivation' ) );
+			register_deactivation_hook( $plugin_path_for_deactivate_cron, array(
+				$this,
+				'disable_cron_on_deactivation'
+			) );
 
 		}
+
 		function deregister_cron_for_module( $module ) {
 			wp_clear_scheduled_hook( 'rt_parse_email_cron', array( $module ) );
 			wp_clear_scheduled_hook( 'rt_send_email_cron' );
-			remove_action( 'rt_parse_email_cron',  array( $this, 'rt_parse_email' ) );
+			remove_action( 'rt_parse_email_cron', array( $this, 'rt_parse_email' ) );
 			remove_action( 'rt_send_email_cron', array( $this, 'rt_send_email' ) );
 		}
 
 		function register_custom_schedule( $schedules ) {
 			// add schedule to the existing set
-			$schedules['every_minute'] = array(
+			$schedules['every_minute']    = array(
 				'interval' => 60,
-				'display' => __( 'Every Minute' ),
+				'display'  => __( 'Every Minute' ),
 			);
 			$schedules['every_5_minutes'] = array(
 				'interval' => 300,
-				'display' => __( 'Every 5 Minutes' ),
+				'display'  => __( 'Every 5 Minutes' ),
 			);
+
 			return $schedules;
 		}
 
@@ -54,7 +59,7 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 			wp_clear_scheduled_hook( 'rt_parse_email_cron' );
 		}
 
-		function setup_schedule(){
+		function setup_schedule() {
 			add_action( 'rt_parse_email_cron', array( $this, 'rt_parse_email' ) );
 			add_action( 'rt_send_email_cron', array( $this, 'rt_send_email' ) );
 			if ( ! wp_next_scheduled( 'rt_parse_email_cron' ) ) {
@@ -72,7 +77,7 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 		 */
 		function rt_parse_email() {
 
-			global $rt_mail_settings, $rt_mail_accounts_model ;
+			global $rt_mail_settings, $rt_mail_accounts_model;
 
 			$emails = $rt_mail_accounts_model->get_all_mail_accounts();
 			foreach ( $emails as $emailRow ) {
@@ -128,11 +133,11 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 				// skip sending email filter.
 				$do_send = apply_filters( 'rt_lib_before_sending_email', true, $email );
 				if ( ! $do_send ) {
-					rt_log( 'Skip email : '. var_export( $email->id, true ) );
+					rt_log( 'Skip email : ' . var_export( $email->id, true ) );
 					$rt_mail_settings->update_sent_email( $email->id, 'skip', 'no' );
 					continue;
 				}
-				rt_log( 'Sending email : '. var_export( $email->id, true ) );
+				rt_log( 'Sending email : ' . var_export( $email->id, true ) );
 				if ( ! isset( $accessTokenArray[ $email->fromemail ] ) ) {
 					$email_type                            = '';
 					$imap_server                           = '';
@@ -147,8 +152,8 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 					$updateFlag = false;
 					try {
 						$fromname = ( ! empty( $email->fromname ) ) ? $email->fromname : get_bloginfo();
-						$result = $rtZendEmail->sendemail( $fromname, $email->fromemail, $accessTokenArray[ $email->fromemail ]['token'], $accessTokenArray[ $email->fromemail ]['email_type'], $accessTokenArray[ $email->fromemail ]['imap_server'], $email->subject, $email->body, unserialize( $email->toemail ), unserialize( $email->ccemail ), unserialize( $email->bccemail ), unserialize( $email->attachement ), $email );
-						rt_log( var_export( 'Email id :'.$email->id.' Status :' . $result, true ) );
+						$result   = $rtZendEmail->sendemail( $fromname, $email->fromemail, $accessTokenArray[ $email->fromemail ]['token'], $accessTokenArray[ $email->fromemail ]['email_type'], $accessTokenArray[ $email->fromemail ]['imap_server'], $email->subject, $email->body, unserialize( $email->toemail ), unserialize( $email->ccemail ), unserialize( $email->bccemail ), unserialize( $email->attachement ), $email );
+						rt_log( var_export( 'Email id :' . $email->id . ' Status :' . $result, true ) );
 						if ( $result ) {
 							$updateFlag = true;
 						}
@@ -159,10 +164,10 @@ if ( ! class_exists( 'Rt_Mail_Cron' ) ) {
 						$rt_mail_settings->update_sent_email( $email->id, 'yes', 'p' );
 					} else {
 						$rt_mail_settings->update_sent_email( $email->id, 'error', 'p' );
-						rt_log( 'Error: ' . esc_attr( $email->id ). '<br />' );
+						rt_log( 'Error: ' . esc_attr( $email->id ) . '<br />' );
 					}
 				} else {
-					rt_log( 'Error: ' . esc_attr( $email->id ). '<br />' );
+					rt_log( 'Error: ' . esc_attr( $email->id ) . '<br />' );
 				}
 			}
 		}
